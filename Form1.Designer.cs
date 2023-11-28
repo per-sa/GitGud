f62df90116aaaf078aefb492becb62531eabe55d
Matheus Colombo <matheus.colombo@janeladigital.com>
2023-11-28 15:24:16 +0000

    Styles, Structure Init and Others

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            label2 = new Label();
+            linkLabel1 = new LinkLabel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label1 = new Label();
+            richTextBox1 = new RichTextBox();
+            label3 = new Label();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(14, 111);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(410, 22);
+            textBox1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(14, 93);
+            label2.Name = "label2";
+            label2.Size = new Size(119, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Git Auth Token";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.LinkColor = Color.Lime;
+            linkLabel1.Location = new Point(249, 93);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(175, 15);
+            linkLabel1.TabIndex = 3;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Generate One (GitHub)";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // button1
+            // 
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(14, 166);
+            button1.Name = "button1";
+            button1.Size = new Size(86, 38);
+            button1.TabIndex = 4;
+            button1.Text = "PULL";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(106, 166);
+            button2.Name = "button2";
+            button2.Size = new Size(86, 38);
+            button2.TabIndex = 5;
+            button2.Text = "PUSH";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.ForeColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(246, 166);
+            button3.Name = "button3";
+            button3.Size = new Size(86, 38);
+            button3.TabIndex = 6;
+            button3.Text = "FETCH";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.ForeColor = SystemColors.ActiveCaptionText;
+            button4.Location = new Point(338, 166);
+            button4.Name = "button4";
+            button4.Size = new Size(86, 38);
+            button4.TabIndex = 7;
+            button4.Text = "COMMIT";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 148);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 15);
+            label1.TabIndex = 8;
+            label1.Text = "ACTIONS";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BackColor = SystemColors.ActiveCaptionText;
+            richTextBox1.ForeColor = SystemColors.Info;
+            richTextBox1.Location = new Point(14, 251);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new Size(410, 265);
+            richTextBox1.TabIndex = 9;
+            richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(14, 233);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 10;
+            label3.Text = "LOGS";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(383, 528);
+            BackColor = SystemColors.Desktop;
+            ClientSize = new Size(438, 528);
+            Controls.Add(label3);
+            Controls.Add(richTextBox1);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(linkLabel1);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Font = new Font("Agave Nerd Font Mono", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            ForeColor = SystemColors.ButtonHighlight;
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
             Name = "Form1";
-            Text = "Form1";
+            Text = "GitGud";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label2;
+        private LinkLabel linkLabel1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label1;
+        private RichTextBox richTextBox1;
+        private Label label3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,10 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Diagnostics;
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+
 namespace GitGud
 {
     public partial class Form1 : Form
@@ -6,5 +13,78 @@ namespace GitGud
         {
             InitializeComponent();
         }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Create a new ProcessStartInfo
+            var psi = new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://github.com/settings/tokens",
+                UseShellExecute = true 
+            };
+
+            System.Diagnostics.Process.Start(psi);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Please enter a valid token");
+                return;
+            }
+            else
+            {
+                RunPythonScript();
+            }
+
+        }
+
+        private void RunPythonScript()
+        {
+            try
+            {
+                string python = @"C:\Python312\python.exe";
+                // path to script
+                string myPythonScript = @"C:\Users\main.py";
+
+
+                // Additional arguments to pass to your Python script, if any
+                string args = string.Format("{0}", myPythonScript);
+
+                ProcessStartInfo myProcessStartInfo = new ProcessStartInfo(python)
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    Arguments = args
+                };
+
+                using (Process myProcess = new Process())
+                {
+                    myProcess.StartInfo = myProcessStartInfo;
+                    myProcess.Start();
+
+                    StreamReader myStreamReader = myProcess.StandardOutput;
+                    StringBuilder output = new StringBuilder();
+
+                    while (!myStreamReader.EndOfStream)
+                    {
+                        string line = myStreamReader.ReadLine();
+                        output.AppendLine(line);
+                    }
+
+                    richTextBox1.Invoke(new Action(() => {
+                        richTextBox1.Text = output.ToString();
+                    }));
+
+                    myProcess.WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
     }
 }
